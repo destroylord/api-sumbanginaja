be9ddc838db2017f837262d723dda5ff8bfa1647
destroylord <masapin68@gmail.com>
2021-06-09 13:44:36 +0700

    set route, add food migration and add file seeder,factory, models

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => 'auth:sanctum'], function (Request $request) {
     return $request->user();
 
     Route::get('/test', function(){
